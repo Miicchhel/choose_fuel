b86cbbefa3847b372435ab9413b547567c945c3e
Miicchhel <michel.ferreira.melo@gmail.com>
2023-11-03 17:21:53 -0300

    add regra de negócio

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,7 +1,53 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+
+interface InputsProps {
+    gasolina: number;
+    etanol: number;
+}
+
+interface InfoProps {
+        title: string, 
+        infoGasolina: number,
+        infoEtanol: number
+    }
+
 export default function Formulario() {
+    const [inputs, setInputs] = useState<InputsProps>({gasolina: 0, etanol: 0})
+    const [info, setInfo] = useState<InfoProps>()
+
+    function hundlerInput(event: ChangeEvent<HTMLInputElement>) {
+        const {name, value} = event.target
+        setInputs((prevInputs) => ({...prevInputs, [name]:Number(value)}))
+    }
+
+    function calcular(event: FormEvent){
+        event.preventDefault()
+        
+        const {gasolina, etanol} = inputs
+        const calculo = (etanol / gasolina)
+        
+        if (calculo <= 0.7) {
+            setInfo(
+                {
+                    title: 'Compensa usar etanol', 
+                    infoGasolina: inputs.gasolina,
+                    infoEtanol: inputs.etanol
+                }
+            )
+        } else {
+            setInfo(
+                {
+                    title: 'Compensa usar gasolina', 
+                    infoGasolina: inputs.gasolina,
+                    infoEtanol: inputs.etanol
+                }
+            )
+        }
+    }
+
     return(
-        <div>
-            <form className='form'>
+        <>
+            <form className='form' onSubmit={calcular}>
                 <label>R$ Gasolina/Litro</label>
                 <input
                     type="number"
@@ -10,6 +56,9 @@ export default function Formulario() {
                     min={1}
                     step={0.01}
                     required
+                    name="gasolina"
+                    value={inputs.gasolina}
+                    onChange={hundlerInput}
                 />
 
                 <label>R$ Etanol/Litro</label>
@@ -20,10 +69,13 @@ export default function Formulario() {
                     min={1}
                     step={0.01}
                     required
+                    name="etanol"
+                    value={inputs.etanol}
+                    onChange={hundlerInput}
                 />
+                <button type="submit">Calcular</button>
             </form>
 
-            <button>Calcular</button>
-        </div>
+        </>
     )
 }
